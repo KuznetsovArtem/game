14d205c9cfcd66963b2682d9e254e19f6b16853a
Artem <artem@gypost.com>
2014-07-24 16:55:51 +0300

    editor clean up, draw, zoom

--- public/editor/editor.js
+++ public/editor/editor.js
@@ -11,36 +11,28 @@ var constuct =  function(map) {
         H: 800,
         antialising: false
     };
-
-    // init
-    // var camera = new PIXI.Stage(0xAABBCC);
+    // Map and spriteSheet
     var mapStage = new PIXI.Stage('', true); // interactive
-    var spriteStage = new PIXI.Stage('', true);
-
-    // var stage = new PIXI.DisplayObjectContainer();
+    var spriteStage = new PIXI.Stage('');
 
     var renderer = PIXI.autoDetectRenderer(conf.W, conf.H, null, true);
     document.getElementById('map-editor').appendChild(renderer.view);
-    // document.body.appendChild(renderer.view);
+
     var sheetRenderer = PIXI.autoDetectRenderer(160, 160, null, true);
     document.getElementById('sprite-sheet').appendChild(sheetRenderer.view);
 
-
+    // Terrain itit
     var mapTerrainLayer = new PIXI.DisplayObjectContainer();
     mapStage.addChild(mapTerrainLayer);
+    window.terrain = mapTerrainLayer;
+    // init zoom for tarrain lalyer
+    mapFx.zoom(mapTerrainLayer);
 
-    var mapWidth = map.terrain.length;
-    var mapHeight = map.terrain[0].length;
-    for(var i = 0; i < mapWidth; i++) {
-        for(var j = 0; j < mapHeight; j++) {
-            var graphic = new PIXI.Graphics();
-            graphic.lineStyle(1, 0xfafafa);
-            graphic.drawRect(i * map.tileW, j * map.tileH, map.tileW, map.tileH);
-            mapTerrainLayer.addChild(graphic);
-        }
-    }
+    // Grid for clearance
+    mapFx.drawGrid(map.terrain.length, map.terrain[0].length, map, mapTerrainLayer);
 
-    var texture = PIXI.Texture.fromImage("/editor/tiles/etst/t1.png");
+/* // Tile test
+   var texture = PIXI.Texture.fromImage("/editor/tiles/etst/t1.png");
     // create a new Sprite using the texture
     var tile = new PIXI.Sprite(texture);
     // center the sprites anchor point
@@ -50,21 +42,20 @@ var constuct =  function(map) {
     tile.position.x = 200;
     tile.position.y = 150;
     tile.interactive = true
-
     tile.mousedown = function(data) {
         console.log('click', data)
-        tile.mousemove = function(e) {
-            console.log('move', e)
-            tile.position.x = e.originalEvent.x - 320;
-            tile.position.y = e.originalEvent.y - 40;
-        }
+        // dragging
+//        tile.mousemove = function(e) {
+//            console.log('move', e)
+//            tile.position.x = e.originalEvent.x - 320;
+//            tile.position.y = e.originalEvent.y - 40;
+//        }
         tile.mouseup = function() {
             tile.mousemove = function() {}
         }
     }
-
     mapStage.addChild(tile);
-
+*/
 
     requestAnimFrame(animate);
 
@@ -83,7 +74,54 @@ var constuct =  function(map) {
  *
  */
 var mapFx = (function() {
+    fx = {
+        makeSquere: function(map) {
+            graphic = new PIXI.Graphics();
+            graphic.lineStyle(1, 0xfafafa);
+            graphic.drawRect(0, 0, map.tileW -10, map.tileH -10);
+            return graphic;
+        },
+        zoomLayer: ''
+    }
+
+    function MapTerSprite(index, sprite) {
+        this.index = {
+            x: index.x||0,
+            y: index.y||0
+        };
+
+        var parent = this;
+        this.sprite = sprite||'';
+        this.sprite.self = parent;
+    }
+    MapTerSprite.prototype.getIndex = function() {
+        return this.index;
+    }
+
     return {
+        test: function() {
+            var sp1 = new MapTerSprite({x: 2, y:4});
+            var sp2 = new MapTerSprite({x: 25, y:14});
+            console.log(sp1, sp1.getIndex(), sp2, sp2.getIndex());
+        },
+        zoom : function(obj) {
+            var layer = fx.zoomLayer;
+            if(obj) {
+                fx.zoomLayer  = obj;
+                return ;
+            } else {
+                return {
+                    in: function() {
+                        layer.scale.x += 0.5
+                        layer.scale.y += 0.5
+                    },
+                    out: function() {
+                        layer.scale.x -= 0.5
+                        layer.scale.y -= 0.5
+                    }
+                }
+            }
+        },
         generateEmptyLayer: function (w, h) {
             var layer = [];
             for(var i = 0; i < w; i++) {
@@ -93,6 +131,38 @@ var mapFx = (function() {
                 }
             }
             return layer;
+        },
+        drawGrid: function(mapWidth, mapHeight, map, mapLayer) {
+            var terrain = [];
+            for(var i = 0; i < mapWidth; i++) {
+                terrain[i] = [];
+                for(var j = 0; j < mapHeight; j++) {
+                    terrain[i][j] = new MapTerSprite({
+                        x: i,
+                        y: j
+                    }, new PIXI.Sprite(fx.makeSquere(map).generateTexture()));
+                    var sprite = terrain[i][j].sprite;
+                    sprite.position.x = i * map.tileW;
+                    sprite.position.y = j * map.tileH;
+                    sprite.anchor.x = 0;
+                    sprite.anchor.y = 0;
+                    sprite.interactive = true
+                    sprite.mousedown = function(e) {
+                        console.log(this, "clicked on sprite", e);
+                        var brushTexture = brush.get()||"/editor/tiles/etst/t1.png"
+                        this.texture = PIXI.Texture.fromImage(brushTexture);
+                    }
+                    sprite.mouseover = function(e) {
+                        if(e.originalEvent.ctrlKey) {
+                            var brushTexture = brush.get()||"/editor/tiles/etst/t1.png"
+                            this.texture = PIXI.Texture.fromImage(brushTexture);
+                            console.log("drawing..");
+                        }
+                    }
+                    mapLayer.addChild(sprite);
+                }
+            }
+            //
         }
     }
 })();
@@ -121,9 +191,6 @@ function createMap() {
 
 function saveMap() {
     console.log('try to save');
-//    $.get('/map/' + mapObject.name, function(data) {
-//        console.log(data);
-//    });
 
     var data = {
         map: mapObject,
@@ -143,3 +210,17 @@ function saveMap() {
     })
 
 }
+
+
+var brush = (function() {
+    var current;
+    return {
+        set : function(pic) {
+            current = pic;
+            console.log('brush set to: ' + current);
+        },
+        get : function() {
+            return current;
+        }
+    }
+})();
